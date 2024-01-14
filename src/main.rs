88b9f1979b5958e9621facada5e85ce02a62f2da
kurorinchan <kurorinchan@users.noreply.github.com>
2024-01-14 10:43:25 +0900

    Add number input to change hits
    Also add styling.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,44 @@ use leptos::*;
 fn RatioTable() -> impl IntoView {
     let init_val = 1;
     let (value, set_value) = create_signal(init_val);
+    let on_slider_change = move |ev| {
+        let ev = event_target_value(&ev);
+        let int_val = ev.parse::<i32>().unwrap_or(1);
+        set_value.update(|v| {
+            *v = int_val;
+        });
+    };
+
+    let on_input_change = move |ev| {
+        let value = event_target_value(&ev);
+        if value.is_empty() {
+            return;
+        }
+        let value = value.parse::<i32>().unwrap_or(1);
+        set_value.set(value);
+    };
 
     view! {
         <div>
-            <input type="range" name="hit_count" value={move||init_val} min={move||init_val} max=20 on:input=move |ev|{
-                let ev = event_target_value(&ev);
-                let int_val = ev.parse::<i32>().unwrap_or(1);
-                set_value.update(|v| {
-                    *v = int_val;
-                });
-            }
+            # of hits
+        </div>
+        <div>
+            <input
+                type="number"
+                prop:value=value
+                on:input=on_input_change
             />
-            <label for="hit_count">hits {value}</label>
+        </div>
+        <div>
+            <input
+                type="range"
+                name="hit_count"
+                prop:value={value}
+                min={init_val}
+                max=20
+                on:input=on_slider_change
+            />
+            //<label for="hit_count">hits {value}</label>
         </div>
         <Table hits={value}/>
     }
@@ -34,32 +60,36 @@ fn PercentTableData(hits: ReadSignal<i32>, non_emit_prob: f64) -> impl IntoView 
     let percentage = move || 100.0 * (1.0 - non_emit_prob.powi(hits.get()));
 
     let class = move || {
-        let mut classes = vec!["has-text-right"];
+        let mut classes = vec!["text-right"];
         if percentage() > 70.0 {
-            classes.push("is-success");
+            classes.push("text-bg-success");
         }
 
         classes.join(" ")
     };
 
     view! {
-        <td class="has-text-right" class={class}>{move || {
-            format!("{:.2}%", percentage())
+        <td class={class}>{move || {
+
+            let mut percentage = format!("{:.1}", percentage());
+            if percentage == "100.0" {
+                percentage = "100".to_string();
+            }
+            format!("{percentage}%")
         }}</td>
     }
 }
 
 #[component]
 fn TableBody(hits: ReadSignal<i32>) -> impl IntoView {
-    let table_data = no_particle_table();
+    let another_table = no_particle_table();
 
     view! {
-        <th rowspan="6" scope="rowgroup" class="is-centered is-vcentered">精錬ランク</th>
-        {table_data.into_iter().enumerate().map(
+        {another_table.into_iter().enumerate().map(
             |(i, row)| {
                 view! {
                     <tr>
-                        <th scope="row" class="is-info">R{i+1}</th>
+                        <th scope="row" class="text-bg-info text-right">R{i+1}</th>
                         {row.into_iter().map(
                             |item| {
                                 view! {
@@ -78,19 +108,18 @@ fn TableBody(hits: ReadSignal<i32>) -> impl IntoView {
 #[component]
 fn Table(hits: ReadSignal<i32>) -> impl IntoView {
     view! {
-        <table class="table is-striped is-hoverable is-bordered">
+        <table class="table table-striped table-bordered">
             <thead>
                 <tr>
                     <th scope="colgroup" colspan="2"  />
-                    <th scope="colgroup" colspan="10" class="has-text-centered">会心率</th>
+                    <th scope="colgroup" colspan="10" class="text-center">会心率</th>
                 </tr>
                 <tr>
-                    <th />
                     <th />
                         {
                             (10..=100).step_by(10).map(|percent| {
                                 view!{
-                                    <th scope="col" class="is-info has-text-right">{percent}%</th>
+                                    <th scope="col" class="text-bg-info text-right">{percent}%</th>
                                 }
                             }
                             ).collect_view()
